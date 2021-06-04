9997bdacb6b435aa83a4533ca8c9889dc048f746
Christopher Susie <cpsusie@hotmail.com>
2021-06-04 17:53:25 -0400

    Elaborating concepts

--- cjm_synchro/cjm_synchro.cpp
+++ cjm_synchro/cjm_synchro.cpp
@@ -5,22 +5,25 @@
 #include <iostream>
 #include "cjm_synchro_concepts.hpp"
 #include <chrono>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/shared_mutex.hpp>
+
+
 int main()
 {
 	using namespace cjm::synchro::concepts;
 	constexpr auto newl = '\n';
 	std::cout << "Hello World!" << newl;
 
-	std::mutex m;
-	m.lock();
-	m.unlock();
-	m.try_lock();
 	auto tp = std::chrono::steady_clock::now();
 	auto tp2 = std::chrono::steady_clock::now();
 	auto dur = tp2 - tp;
 	static_assert(time_point<decltype(tp)>);
 	static_assert(duration < decltype(dur) > );
-	
+
+	static_assert(mutex<boost::mutex>);
+	static_assert(mutex<boost::shared_mutex>);
+	static_assert(mutex<boost::upgrade_mutex>);
 	static_assert(basic_lockable<std::mutex>);
 	static_assert(mutex<std::mutex>);
 	static_assert(mutex<std::recursive_mutex>);
@@ -28,8 +31,32 @@ int main()
 	static_assert(mutex<std::timed_mutex>);
 	static_assert(mutex<std::shared_timed_mutex>);
 	static_assert(mutex<std::recursive_timed_mutex>);
+
+	static_assert(timed_mutex<boost::timed_mutex, boost::chrono::microseconds, boost::chrono::steady_clock::time_point>);
+	static_assert(timed_mutex<boost::shared_timed_mutex, boost::chrono::microseconds, boost::chrono::steady_clock::time_point>);
+	static_assert(timed_mutex<std::timed_mutex, std::chrono::microseconds, std::chrono::steady_clock::time_point>);
+	static_assert(timed_mutex<std::recursive_timed_mutex, std::chrono::microseconds, std::chrono::steady_clock::time_point>);
+	static_assert(timed_mutex<std::shared_timed_mutex, std::chrono::microseconds, std::chrono::steady_clock::time_point>);
+	static_assert(!timed_mutex<std::timed_mutex, short, std::chrono::steady_clock::time_point>);
+	static_assert(!timed_mutex<std::timed_mutex, std::chrono::microseconds, int>);
+	static_assert(!timed_mutex<std::mutex, std::chrono::microseconds, std::chrono::steady_clock::time_point>);
+	static_assert(!timed_mutex<std::recursive_mutex, std::chrono::microseconds, std::chrono::steady_clock::time_point>);
+	static_assert(!timed_mutex<std::shared_mutex, std::chrono::microseconds, std::chrono::steady_clock::time_point>);
+
+	static_assert(shared_mutex<std::shared_mutex>);
+	static_assert(shared_mutex<std::shared_timed_mutex>);
+	static_assert(shared_mutex<boost::shared_mutex>);
+	static_assert(!shared_mutex<std::mutex>);
+	static_assert(!shared_mutex<std::timed_mutex>);
+	static_assert(!shared_mutex<boost::mutex>);
+	static_assert(shared_mutex<boost::upgrade_mutex>);
+	
+	
+	return 0;
 	
 }
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
@@ -41,3 +68,5 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
--- cjm_synchro/cjm_synchro_concepts.hpp
+++ cjm_synchro/cjm_synchro_concepts.hpp
@@ -3,7 +3,8 @@
 #include <chrono>
 #include <concepts>
 #include <type_traits>
-
+#include <mutex>
+#include <condition_variable>
 namespace cjm::synchro::concepts
 {
 	namespace detail
@@ -41,17 +42,47 @@ namespace cjm::synchro::concepts
 
 	template<typename TLockable>
 	concept lockable = basic_lockable<TLockable>
-		&& requires (TLockable l)
+		&& requires (TLockable& l)
 	{
 		{l.try_lock()} -> detail::nothrow_convertible_to<bool>;
 	};
 
+	template<typename TSharedLockable>
+	concept shared_lockable = requires (TSharedLockable& sl)
+	{
+		{sl.lock_shared() };
+		{sl.try_lock_shared()} -> detail::nothrow_convertible_to<bool>;
+		{sl.unlock_shared()};
+	};
+
+	template<typename TUpgradeLockable>
+	concept upgrade_lockable = shared_lockable<TUpgradeLockable> && requires (TUpgradeLockable & up)
+	{
+		{up.lock_upgrade()};
+		{up.unlock_upgrade()};
+		{up.try_lock_upgrade()} -> detail::nothrow_convertible_to<bool>;
+		{up.unlock_and_lock_shared()};
+		{up.unlock_and_lock_upgrade()};
+		{up.unlock_upgrade_and_lock()};
+		{up.unlock_upgrade_and_lock_shared()};
+		{up.try_unlock_upgrade_and_lock()} -> detail::nothrow_convertible_to<bool>;
+	};
+
 	template<typename TTimedLockable, typename TDuration, typename TTimePoint>
 	concept timed_lockable = lockable<TTimedLockable> &&  duration<TDuration> && time_point<TTimePoint> &&
-		requires (TTimedLockable tl, const TDuration & dur, const TTimePoint & tp)
+		requires (TTimedLockable& tl, const TDuration & dur, const TTimePoint & tp)
 	{
 		{tl.try_lock_for(dur)} -> detail::nothrow_convertible_to<bool>;
 		{tl.try_lock_until(tp)} -> detail::nothrow_convertible_to<bool>;
+	};
+
+	template<typename TTimedSharedLockable, typename TDuration, typename TTimePoint>
+	concept shared_timed_lockable = lockable<TTimedSharedLockable> && timed_lockable<TTimedSharedLockable, TDuration, TTimePoint> && 
+		shared_lockable<TTimedSharedLockable>
+			&& requires (TTimedSharedLockable& tl, const TDuration& dur, const TTimePoint& tp)
+	{
+		{tl.try_lock_shared_for(dur)} -> detail::nothrow_convertible_to<bool>;
+		{tl.try_lock_shared_until(tp)} -> detail::nothrow_convertible_to<bool>;
 	};
 
 	template<typename TMutex>
@@ -62,6 +93,33 @@ namespace cjm::synchro::concepts
 					&& !std::is_copy_assignable_v<TMutex> && 
 					!std::move_constructible<TMutex> && 
 					!std::is_move_assignable_v<TMutex>;
+
+	template<typename TSharedMutex>
+	concept shared_mutex = 
+		mutex<TSharedMutex> && shared_lockable<TSharedMutex>;
+
+	template<typename TTimedMutex, typename TDuration, typename TTimePoint>
+	concept timed_mutex = mutex<TTimedMutex> && timed_lockable<TTimedMutex, TDuration, TTimePoint>;
+
+	template<typename TTimedSharedMutex, typename TDuration, typename TTimePoint>
+	concept timed_shared_mutex = 
+		shared_mutex<TTimedSharedMutex> && timed_mutex<TTimedSharedMutex, TDuration, TTimePoint>;
+
 	
+	
+	template<mutex TMutex>
+	struct mutex_traits
+	{
+		template<duration TDuration, time_point TTime>
+		static constexpr bool supports_exclusive_timed_locks_with = timed_mutex<TMutex, TDuration, TTime>;
+		static constexpr bool is_shared = shared_mutex<TMutex>;
+		static constexpr bool is_timed = timed_mutex<TMutex>;
+		static constexpr bool is_upgrade = upgrade_mutex<TMutex>;
+		static constexpr bool supports_condition_variable = std::is_same_v<std::remove_cvref_t<TMutex>, std::mutex>;
+
+		
+		
+		
+	};
 };
 #endif
